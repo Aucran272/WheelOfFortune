5faa0049c70c0b40e3bfedc3f467c9999cc8f0fe
afc5125 <afc5125@OZSTHLND307-99.win.psu.edu>
2016-09-16 11:50:29 -0400

    Added input of a letter and checking if it is an actual letter that's being inputted

--- src/wheeloffortune/WheelOfFortune.java
+++ src/wheeloffortune/WheelOfFortune.java
@@ -1,5 +1,5 @@
 /*
-* CMPSC 261, Section 1
+* IST 261, Section 1
 * Fall 2016
 * Instructor: Phil O'Connell
 * TODO: DON'T FORGET TO PUT YOUR NAME AND ID
@@ -8,13 +8,44 @@
 */
 package wheeloffortune;
 
-public class WheelOfFortune {
+import java.util.Scanner;
 
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String[] args) {
+public class WheelOfFortune {
     
+
+  public static void main(String[] args) {
+      input();
+  }
+  
+  public static void input(){
+    String input;
+    String letter;
+    Scanner scanner = new Scanner(System.in);
+    
+      System.out.println("Welcome to Wheel of Fortune!\n Type 'Spin' to spin the wheel.\n"
+              + "Type 'Vowel' to buy a vowel.\n Type 'Solve' to solve the puzzle.\n"
+              + "Type 'Test' to be a QA Tester.\n Type 'Quit' to quit.");
+      input = scanner.next();
+      System.out.println(input);
+      
+      while (input != "0"){
+          if ("Spin".equals(input)){
+              input = "You've spun the wheel.";
+              System.out.println(input);
+              System.out.println("Pick a letter");
+              letter = scanner.next();
+                if(letter.matches("[A-z]")){
+                    System.out.println("You picked the letter " +  letter);
+                    input();
+                   
+                }
+                else {
+                    System.out.println("I'm sorry but that's not a letter,"
+                    + "try entering an actual letter. ");
+                    input();
+                            }
+          }
+      }
   }
   
 }
